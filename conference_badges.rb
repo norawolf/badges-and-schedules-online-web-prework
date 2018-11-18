a85a52a640b9a5f3ca5862a311d93a621cc2f485
Nora Vogt <noraevogt@gmail.com>
2018-11-18 22:46:57 +0000

    automatically backed up by learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -3,7 +3,7 @@ def badge_maker(name)
   "Hello, my name is #{name}."
 end
 
-def_batch_badge_creator(attendees)
+def batch_badge_creator(attendees)
   7.times do
     "Hello, my name is #{attendees}."
   end
