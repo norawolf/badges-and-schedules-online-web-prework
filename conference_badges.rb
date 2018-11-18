5f9cd2bb724f3f438119d7beffc40003c5c40309
Nora Vogt <noraevogt@gmail.com>
2018-11-18 23:05:58 +0000

    automatically backed up by learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -1,4 +1,5 @@
-# Write your code here.
+require 'pry'
+
 def badge_maker(name)
   "Hello, my name is #{name}."
 end
@@ -9,5 +10,17 @@ def batch_badge_creator(attendees)
   attendees.each do |name|
     guests.push("Hello, my name is #{name}.")
   end
+  
   guests
+  end
+
+  def assign_rooms(attendees)
+    name_and_room = []
+
+    attendees.each_with_index do |index, name|
+      name_and_room.push("Hello, #{name}! You'll be assigned to room #{index}!")
+    end
+    name_and_room
+    
+  binding.pry  
   end
